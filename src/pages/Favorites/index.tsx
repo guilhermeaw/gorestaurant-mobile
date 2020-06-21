e732f103112d13ace4ed482fb9fc497e3a1def38
guilhermeaw <guilherme.w@universo.univates.br>
2020-06-20 22:03:46 -0300

    Challenge finished

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -32,7 +32,16 @@ const Favorites: React.FC = () => {
 
   useEffect(() => {
     async function loadFavorites(): Promise<void> {
-      // Load favorite foods from api
+      const response = await api.get<Food[]>('favorites');
+
+      const foods = response.data;
+
+      const formattedPriceFoods = foods.map(food => ({
+        ...food,
+        formattedPrice: formatValue(food.price),
+      }));
+
+      setFavorites(formattedPriceFoods);
     }
 
     loadFavorites();
